385125de87e9a7b6174d0c518b735d544e9ebd24
sunsun1188 <eunsun08110811@gmail.com>
2023-10-05 16:42:29 +0900

    실습2

--- main.c
+++ main.c
@@ -4,19 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
+
 	int num;
 	
 	printf("정수 하나를 입력하시오: ");
 	scanf("%d", &num);
 	
-	if (num>0)
-		printf("양수입니다.");
-	else if (num<0)
-		printf("음수입니다.");
+	if (num<0)
+		printf("절댓값은 %d", -num);
 	else
-		printf("0 입니다.");
-	
+		printf("절댓값은 %d", num);
 	
 	return 0;
 }
