af391b9d95f11e20575d85ec11b1c177052790d9
sunsun1188 <eunsun08110811@gmail.com>
2023-10-08 19:39:40 +0900

    실습4

--- main.c
+++ main.c
@@ -4,18 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-
-	char c;
-	int num=0;
 	
-	while ( (c=getchar())!='\n') {
+	int sum=0;
+	int num,i;
+	
+	printf("Input a number: ");
+	scanf("%d",&num);
+	
+	for (i=0; i<=num; i++) {
 		
-		if (c>='0'&&c<='9') {
-			num++;
-		}
-	}
-	
-	printf("숫자 문자  개수: %d\n",num);
+		sum=sum+i;
+	} 
+	printf("The result is %d",sum);
 		
 	return 0;
 }
